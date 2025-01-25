50fa6f1b7434b11a69954328e1d97362ecfda3d8
marcelomijas (Marcelo Moreno) <marcelomijas@gmail.com>
2025-01-25 14:07:51 +0000

    Added cointegration statistics for cajorls
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urca@112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cajorls.R
+++ pkg/R/cajorls.R
@@ -27,10 +27,40 @@ cajorls <- function(z, r = 1, reg.number = NULL){
         }
         form1 <- formula(paste("z@Z0[, reg.number]", text2, "-1"))
         rlm <- lm(substitute(form1), data = data.mat)
+		return(list(rlm = rlm, beta = betanorm))
     }
     else if (is.null(reg.number)) {
         form1 <- formula(paste("z@Z0", text2, "-1"))
         rlm <- lm(substitute(form1), data = data.mat)
+		# compute beta statistics
+		ZK2 <- z@ZK[, -r] # Y(-1) (2)
+		M <- diag(nrow(z@ZK)) - z@Z1 %*% solve(crossprod(z@Z1)) %*% t(z@Z1)
+		alpha <- matrix(coef(rlm)[1:r, ], ncol = r, byrow = T) # the coefficients on ecm
+		rownames(alpha) <- colnames(coef(rlm)[1:r, ])
+		SIGMA <- crossprod(resid(rlm)) / nrow(z@ZK)
+		beta.se <- sqrt(diag(kronecker(
+		  solve(t(ZK2) %*% M %*% ZK2),
+		  solve(t(alpha) %*% solve(SIGMA) %*% alpha)
+		)))
+		beta.se <- matrix(c(rep(NA, r), beta.se), ncol = r, byrow = T)
+		for (i in 1:r) {
+		  beta.se[i, ] <- NA
+		}
+		rownames(beta.se) <- rownames(beta)
+		colnames(beta.se) <- colnames(beta)
+		beta.t <- betanorm / beta.se
+		beta.pval <- 2 * pt(abs(beta.t), df = rlm$df.residual, lower.tail = F)
+		if (r == 1) {
+		  beta.stats <- data.frame(cbind(betanorm, beta.se, beta.t, beta.pval))
+		  colnames(beta.stats) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+		} else {
+		  beta.stats <- list()
+		  for (ect in 1:ncol(beta)) {
+			beta.ect <- data.frame(cbind(round(betanorm[, ect], digits = 15), beta.se[, ect], beta.t[, ect], beta.pval[, ect]))
+			colnames(beta.ect) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+			beta.stats[[ect]] <- beta.ect
+		  }
+		}
+		return(list(rlm = rlm, beta = betanorm, beta.stats = beta.stats))
     }
-    return(list(rlm = rlm, beta = betanorm))
 }
